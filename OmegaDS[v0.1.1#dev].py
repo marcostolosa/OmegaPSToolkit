7d906c0f5329cdf85dca0da805934b8712aa8315
Meep <82945541+th300905@users.noreply.github.com>
2021-10-18 23:42:51 +0200

    Add files via upload

---
+++ OmegaDS[v0.1.1#dev].py
@@ -0,0 +1,30 @@
+ #-Metadata----------------------------------------------------#
+ #  Filename: DS-ToolBox[v0.1.4#dev]      [Update: 12-10-2021] #
+ #-Info--------------------------------------------------------#
+ #  DS-ToolBox - A basic toolbox for windows totaly            #
+ #  Language   - Python3
+ #-Author[s}---------------------------------------------------#
+ #  Thomas Pellissier ~ @$_M33p                                #
+ #-Operating System--------------------------------------------#
+ #  Designed for & tested on: Windows                          #
+ #                                                             #
+ #  Reported working : All Windows version [MS-DOS]            #
+ #-Licence-----------------------------------------------------#
+ #  GNU General Public License v3.0                            #
+ #-------------------------------------------------------------#
+ 
+ 
+print("       _______  _______  _______  _______  _______          _____   _______ ")
+print("      |       ||   |   ||    ___||     __||   _   | ______ |     \ |     __|")
+print("      |   -   ||       ||    ___||    |  ||       ||______||  --  ||__     |")
+print("      |_______||__|_|__||_______||_______||___|___|        |_____/ |_______|")                                                                
+print("     ~~~~~~~~~~~~~~~~~~~~~~~~~~ © Delta_Society™ ~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+print("     [##]            ToolBox factory for penetration testing            [##]")
+print("     [##]       Created by : Thomas Pellissier (© Delta_Society™)       [##]")
+print("     [##]                        Version : 0.0.1                        [##]")
+print("     [##]                       Codename : $_M33p                       [##]")
+print("     [##]               Designed for & tested on Windows                [##]")
+print()
+print("     [01]    |    Fyodor all tools")
+print("     [02]    |    Windows tools")
+print("     [03]    |    Wireless attack")
