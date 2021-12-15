89e7b02e560f1d5da019eda907ef1b6b897c4a54
Thomas Pellissier <82945541+MyMeepSQL@users.noreply.github.com>
2021-12-15 20:14:03 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='OmegaDSToolkit[v0.0.0.6#dev]',
+   version='0.0.0.6',
+   description='Password system',
+   author='Déodorant',
+   author_email='thomas.pellissier@outlook.com',
+   url='https://github.com/MyMeepSQL/OmegaDSToolkit',
+   packages=['OmegaDSToolkit'],
+   install_requires=['colored', 'progress'],
+)
