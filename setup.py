807bc96d4a0deb1a5afb4d56f4eb97257571ff57
Thomas Pellissier <82945541+MyMeepSQL@users.noreply.github.com>
2021-12-16 15:48:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 setup(
+   install_requires=['colored', 'progress'],
    name='OmegaDSToolkit[v0.0.0.6#dev]',
    version='0.0.0.6',
-   description='Password system',
-   author='Déodorant',
-   author_email='thomas.pellissier@outlook.com',
    url='https://github.com/MyMeepSQL/OmegaDSToolkit',
-   packages=['OmegaDSToolkit'],
-   install_requires=['colored', 'progress'],
+   description='A massive penetration testing toolkit',
+   author='MyMeepSQL',
+   author_email='thomas.pellissier@outlook.com',
 )
