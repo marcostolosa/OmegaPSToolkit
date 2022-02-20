6f04a49481dd2bee67d59464cc1e6c28eb6992c7
Thomas Pellissier <82945541+MyMeepSQL@users.noreply.github.com>
2022-02-20 23:01:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
-#---[Metadata]-----------------------------------------------------#
-#  Filename: setup.py                         [Update: 17-01-2022] #
-#---[Info]---------------------------------------------------------#
-#  The setup for had all modules that the ODST use                 #
-#  Language      - Python3                                         #
-#---[Author]-------------------------------------------------------#
-#  Thomas Pellissier ~ @MyMeepSQL                                  #
-#------------------------------------------------------------------#
+#---[Metadata]------------------------------------------------------#
+#  Filename: setup.py                          [Update: 20-02-2022] #
+#---[Info]----------------------------------------------------------#
+#  {The OmegaDSToolkit is a product of Delta_Society™ by MyMeepSQL} #
+#                                                                   #
+#  The setup of ODST                                                #
+#  Language  ~  Python3                                             #
+#---[Author]--------------------------------------------------------#
+#  Thomas Pellissier ~ @MyMeepSQL                                   #
+#-------------------------------------------------------------------#
+
 
 from setuptools import setup, find_packages
 from time import sleep
 print()
-print("""  /$$$$$$              /$$                      /$$$$$$$$                  /$$
+print("""
+  /$$$$$$              /$$                      /$$$$$$$$                  /$$
  /$$__  $$            | $$                     |__  $$__/                 | $$
 | $$  \__/  /$$$$$$  /$$$$$$   /$$   /$$  /$$$$$$ | $$  /$$$$$$   /$$$$$$ | $$
 |  $$$$$$  /$$__  $$|_  $$_/  | $$  | $$ /$$__  $$| $$ /$$__  $$ /$$__  $$| $$
@@ -22,32 +26,44 @@ print("""  /$$$$$$              /$$                      /$$$$$$$$              
                                         | $$                                  
                                         |__/
 """,end="")
-print("========================= Welcome to the ODST setup. =========================")
+print("========================= Welcome to the ODST setuptool. =========================")
 sleep(1.2)
 print()
-setup(name='OmegaDSToolkit',
-      version='0.0.0.6',
-      description='A massive penetration testing toolkit',
-      url='https://github.com/MyMeepSQL/OmegaDSToolkit',
-      author='MyMeepSQL',
-      author_email='thomas.pellissier@outlook.com',
-      license='GNU-GPL-3.0',
-      python_requires='>=3.3.0',
-      packages=find_packages(),
-      include_package_data=True,
-      entry_points={
-          "console_scripts": [
-                "odst = OmegaDSToolkit:main"
-          ]
-      },
-      install_requires=[
-          'progress', 'colored', 'nslookup', 'keyboard',
-          'pythonping'
-      ],
-      zip_safe=False
+
+requirements = ["requests"]
+
+setup_requirements = ["requests"]
+
+test_requirements = ["requests"]
+
+setup(classifiers=[
+        "Development Status                 :: 2 - Production/Stable",
+        "Natural Language                   :: English (little french)",
+        "Environment                        :: Console",
+        "Intended Audience                  :: Developers",
+        "License                            :: GNU-GPL-3.0",
+        "Programming Language               :: Python :: 3.10",
+        "Programming Language compatible    :: Python :: 3.2-3.10",
+    ],
+    name='OmegaDSToolkit',
+    keywords="omegadstoolkit",
+    description='A massive penetration testing toolkit',
+    url='https://github.com/MyMeepSQL/OmegaDSToolkit',
+    author='MyMeepSQL',
+    author_email='thomas.pellissier@outlook.com',
+    license='GNU-GPL-3.0',
+    version='0.0.0.6',
+    python_requires='>=3.3.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'progress', 'colored', 'nslookup', 'keyboard',
+        'pythonping'
+    ],
+    zip_safe=False
 )
+
 print()
-print("<========================================================================================================================>")
-print(' Done! All modules are install, now go to the OmegaDSTookit folder and start it with "python OmegaDSToolkit[v0.0.0.6].py"')
-print("<========================================================================================================================>")
-input("Press [ENTER] key to continue")
+print("<=========================================================================================================================>")
+print(' Done! All modules are install, now go to the OmegaDSTookit folder and start it with "python3 OmegaDSToolkit[v0.0.0.6].py"')
+print("<=========================================================================================================================>")
