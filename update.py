1286b66dd9a7e749bee958a1e37bed1ae86cb43d
Thomas Pellissier <82945541+MyMeepSQL@users.noreply.github.com>
2022-03-04 12:04:57 +0100

    Update update.py

--- update.py
+++ update.py
@@ -1,22 +1,36 @@
-#---[Metadata]-------------------------------------------------------#
-#  Filename ~ update.py                        [Update: 04-03-2022]  #
-#---[Info]-----------------------------------------------------------#
-#  {The OmegaDSToolkit is a product of Delta_Society™ by MyMeepSQL}  #
-#                                                                    #
-#  The update tool for have the latest version of ODST               #
-#  Language  ~  Python3                                              #
-#---[Author]---------------------------------------------------------#
-#  Thomas Pellissier ~ @MyMeepSQL                                    #
-#  Copyright (C) 2022 MyMeepSQL - © Delta_Society™                   #
-#---[Operating System]-----------------------------------------------#
-#  Developed for linux                                               #
-#--------------------------------------------------------------------#
-
 #!/usr/bin/python3
 
-import os
-import sys
-import importlib.util
+#---[Metadata]--------------------------------------------------------------#
+#  Filename ~ update.py                     [Update: 2022-03-04 | 11:51 AM] #
+#---[Info]------------------------------------------------------------------#
+#  {The OmegaDSToolkit is a product of Delta_Society™ by MyMeepSQL}         #
+#                                                                           #
+#  The update tool for have the latest version of ODST                      #
+#  Language  ~  Python3                                                     #
+#---[Author]----------------------------------------------------------------#
+#  Thomas Pellissier ~ @MyMeepSQL                                           #
+#  Copyright (C) 2022 MyMeepSQL - © Delta_Society™                          #
+#---[Operating System]------------------------------------------------------#
+#  Developed for linux                                                      #
+#---[Licence]---------------------------------------------------------------#
+#  GNU General Public License v3.0                                          #
+#  -------------------------------                                          #
+#                                                                           #
+#  This program is free software; you can redistribute it and/or modify     #
+#  it under the terms of the GNU General Public License as published by     #
+#  the Free Software Foundation; either version 2 of the License, or        #
+#  (at your option) any later version.                                      #
+#                                                                           #
+#  This program is distributed in the hope that it will be useful,          #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             #
+#  GNU General Public License for more details.                             #
+#                                                                           #
+#  You should have received a copy of the GNU General Public License along  #
+#  with this program; if not, write to the Free Software Foundation, Inc.,  #
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.              #
+#---------------------------------------------------------------------------#
+
 from functions import *
 
 if os.getuid() != 0:                                                            #   check if the user run update with root privilege
@@ -43,36 +57,40 @@ else:                                                                           
 ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗   ██║   ╚██████╔╝╚██████╔╝███████╗
  ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝   ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝   
  """)   # Police = ANSI Shadow from https://www.coolgenerator.com/ascii-text-generator
-        print("+ ------------------- !* Updating OmegaDSToolkit repository *! ------------------- +")
+        print("+ ------------------- !* UpdateTool for OmegaDSToolkit  *! ------------------- +")
 
         # For OmegaDSToolkit
         print("Update the latest version of OmegaDSToolkit...")
-        os.system("rm -f OmegaDSToolkit.py")    
+
+        # Removing the current locl OmegaDSToolkit
+        os.system("rm -f OmegaDSToolkit.py")
+
+        # Download ODST with wget from github raw
         os.system("wget https://raw.githubusercontent.com/MyMeepSQL/OmegaDSToolkit/main/OmegaDSToolkit.py")
         print("Done\n")
 
         # For the setuptool
         print("Update the latest version of OmegaDSToolkit setuptool...")
-        os.system("rm -f setup.py")              
+
+        # Removing the current locl setuptool
+        os.system("rm -f setup.py")
+
+        # Download setuptool with wget from github raw
         os.system("wget https://raw.githubusercontent.com/MyMeepSQL/OmegaDSToolkit/main/setup.py")
         print("Done\n")
 
         # Apply execute rights
         print("Apply execution rights to files...")
-        os.system('chmod +x OmegaDSToolkit.py')
-        os.system('chmod +x setup.py')
+        os.system('chmod +x OmegaDSToolkit.py')     # for OmegaDSToolkit
+        os.system('chmod +x setup.py')              # for the setuptool
 
         print("Done\n")
-        print('''Update complete! For be sure, run the setup.py with "sudo python3 setup.py install" before running ODST. 
-After, you can run OmegaDSToolkit With "sudo python3 OmegaDSToolkit.py"''')
-
-
-
-
-
+        print('''
+Update complete! For be sure, run the setup.py with "sudo python3 setup.py install" before running ODST. 
+After, you can run OmegaDSToolkit with "sudo python3 OmegaDSToolkit.py"''')
         sys.exit()
 
     except KeyboardInterrupt:
         print()
-        print("CTRL + C detected, stoping the update phase...")
+        print("CTRL + C detected, stoping the update tool...")
         sys.exit()
