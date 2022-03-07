c1a0cde6aedfe03118b002f7e2e83e328a54d8f7
Thomas Pellissier <82945541+MyMeepSQL@users.noreply.github.com>
2022-03-07 14:24:18 +0100

    Update colors.py

--- colors.py
+++ colors.py
@@ -1,3 +1,36 @@
+#!/usr/bin/python3
+
+#---[Metadata]--------------------------------------------------------------#
+#  Filename ~ colors.py                     [Update: 2022-03-04 | 14:21 PM] #
+#---[Info]------------------------------------------------------------------#
+#  {The OmegaDSToolkit is a product of Delta_Society™ by MyMeepSQL}         #
+#                                                                           #
+#  All colors in one file for ODST                                          #
+#  Language  ~  Python3                                                     #
+#---[Author]----------------------------------------------------------------#
+#  Thomas Pellissier ~ @MyMeepSQL                                           #
+#  Copyright (C) 2022 MyMeepSQL - © Delta_Society™                          #
+#---[Operating System]------------------------------------------------------#
+#  Developed for linux                                                      #
+#---[Licence]---------------------------------------------------------------#
+#  GNU General Public License v3.0                                          #
+#  -------------------------------                                          #
+#                                                                           #
+#  This program is free software; you can redistribute it and/or modify     #
+#  it under the terms of the GNU General Public License as published by     #
+#  the Free Software Foundation; either version 2 of the License, or        #
+#  (at your option) any later version.                                      #
+#                                                                           #
+#  This program is distributed in the hope that it will be useful,          #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             #
+#  GNU General Public License for more details.                             #
+#                                                                           #
+#  You should have received a copy of the GNU General Public License along  #
+#  with this program; if not, write to the Free Software Foundation, Inc.,  #
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.              #
+#---------------------------------------------------------------------------#
+
 # Colors
 ## Basic colors
 gray = '\033[30m'
